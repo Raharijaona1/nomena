e2d86aa6ee4359cd4e728565b440e6d2fcbb6037
Raharijaona1 <162964617+Raharijaona1@users.noreply.github.com>
2024-04-16 10:09:45 +0200

    c.php

--- c.php
+++ c.php
@@ -1,6 +1,11 @@
 <?php
+// Chemin du répertoire à afficher
+$chemin = './';
+// Récupération de tous les fichiers et dossiers dans le répertoire
+$elements = glob($chemin . '*');
 
-
-    echo ("bonjour");
-
+// Correction du nom de la variable dans la boucle pour éviter la confusion
+foreach ($elements as $element) {
+    echo basename($element) . "<br>";
+}
 ?>
