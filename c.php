e5253d1ac0a47f27e0e382d9c58ea6cc39f460ee
Raharijaona1 <162964617+Raharijaona1@users.noreply.github.com>
2024-04-16 10:25:42 +0200

    Update c.php

--- c.php
+++ c.php
@@ -1,11 +1,12 @@
 <?php
-// Chemin du répertoire à afficher
-$chemin = './';
-// Récupération de tous les fichiers et dossiers dans le répertoire
-$elements = glob($chemin . '*');
 
-// Correction du nom de la variable dans la boucle pour éviter la confusion
-foreach ($elements as $element) {
-    echo basename($element) . "<br>";
-}
+$chemin = 'index.php';
+
+$contenu = file_get_contents ($chemin);
+
+
+
+    echo $contenu;
+
+
 ?>
